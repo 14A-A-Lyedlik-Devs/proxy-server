3450db2da764ad11c150328ab6ae0636fe390a30
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-03-27 12:24:14 +0200

    hi

---
+++ server.js
@@ -0,0 +1,27 @@
+// load dontenv
+require('dotenv').config();
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/access_token', async (req, res) => {
+  const authUrl = `https://github.com/login/oauth/access_token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${req.query.code}`;
+
+  const response = await fetch(authUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const data = await response.json();
+
+  res.send(data);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
